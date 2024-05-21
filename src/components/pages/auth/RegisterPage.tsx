19e892c422b2bb42b775740b6f387ed44c3be89f
Jorge Reyes <92123185+JorgeArt81@users.noreply.github.com>
2024-05-21 16:29:11 -0500

    Create RegisterPage

---
+++ src/components/pages/auth/RegisterPage.tsx
@@ -0,0 +1,78 @@
+import { NavLink } from 'react-router-dom';
+
+import { AuthLayout } from '@/components/layouts/AuthLayout';
+import { Google } from '@mui/icons-material';
+import { Button, Grid, Link, TextField, Typography } from '@mui/material';
+
+export const RegisterPage = () => {
+  return (
+    <AuthLayout title='Registro'>
+      <form>
+        <Grid container direction='column' spacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              label='Nombre completo'
+              type='username'
+              placeholder='John Wick'
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label='Correo'
+              type='email'
+              placeholder='correo@google.com'
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label='Contraseña'
+              type='password'
+              placeholder='Contraseña'
+              fullWidth
+            />
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+          {/* <Grid item xs={12} display={!!errorMessage ? '' : 'none'}>
+        <Alert severity='error'>
+          {errorMessage}
+        </Alert>
+      </Grid> */}
+          <Grid item xs={12} sm={6}>
+            <Button
+              type='submit'
+              variant='contained'
+              disabled={false}
+              fullWidth>
+              Login
+            </Button>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Button
+              variant='contained'
+              disabled={false}
+              onClick={() => {}}
+              fullWidth>
+              <Google />
+              <Typography sx={{ ml: 1 }}>Google</Typography>
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+      <Grid container direction='row' justifyContent='end'>
+      <Typography sx={{ mr: 1 }}>¿Ya tienes cuenta?</Typography>
+        <Link
+          unstable_viewTransition
+          to={'/login'}
+          component={NavLink}
+          color='inherit'
+          justifyContent='end'>
+          Ingresar aquí
+        </Link>
+      </Grid>
+    </AuthLayout>
+  );
+};
