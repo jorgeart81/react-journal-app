0500b9af456ebfb7f0aa9026ba7376810594f9da
Jorge Reyes <92123185+JorgeArt81@users.noreply.github.com>
2024-05-22 13:09:21 -0500

    Create FloatingActionButton

---
+++ src/components/buttons/FloatingActionButton.tsx
@@ -0,0 +1,26 @@
+import { AddOutlined } from '@mui/icons-material';
+import { IconButton } from '@mui/material';
+
+interface Props {
+  disabled?: boolean;
+  onClick?: React.MouseEventHandler;
+}
+
+export const FloatingActionButton = ({ disabled, onClick }: Props) => {
+  return (
+    <IconButton
+      disabled={disabled}
+      onClick={onClick}
+      size='large'
+      sx={{
+        color: 'white',
+        backgroundColor: 'error.main',
+        ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+        position: 'fixed',
+        right: { xs: 40, sm: 50, xl: 60 },
+        bottom: { xs: 40, sm: 50, xl: 60 },
+      }}>
+      <AddOutlined sx={{ fontSize: { xs: 25, sm: 30 } }} />
+    </IconButton>
+  );
+};
