bbaa061d9fcb8ac6c9152435a3b83915904be106
Jorge Reyes <92123185+JorgeArt81@users.noreply.github.com>
2024-05-25 16:40:52 -0500

    Input file with multiple images selected

--- src/components/views/NoteView.tsx
+++ src/components/views/NoteView.tsx
@@ -3,7 +3,8 @@ import {
   KeyboardEvent,
   useEffect,
   useMemo,
-  useState
+  useRef,
+  useState,
 } from 'react';
 
 import {
@@ -26,6 +27,7 @@ interface Props {
 export const NoteView = ({ uid, note, saveNote }: Props) => {
   const [formState, setFormState] = useState(note);
   const { title, body, imageUrls, date } = formState;
+  const fileInputRef = useRef<HTMLInputElement>(null);
 
   const dateString = useMemo(() => {
     const newDate = new Date(date);
@@ -36,11 +38,11 @@ export const NoteView = ({ uid, note, saveNote }: Props) => {
     setFormState(note);
   }, [note]);
 
-  const { handleSubmit, handleChange } = useFormik<Note>({
+  const { handleSubmit, handleChange, setFieldValue } = useFormik<Note>({
     initialValues: {
       body: body || '',
       date: date || 0,
-      imageUrls: imageUrls || [],
+      imageUrls: [],
       title: title || '',
     },
     onSubmit: (values: Note) => {
@@ -50,11 +52,17 @@ export const NoteView = ({ uid, note, saveNote }: Props) => {
 
   const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
+
     handleChange(e);
     setFormState({
       ...formState,
       [name]: value,
     });
+  };
+
+  const onFileInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files?.length === 0) return;
+    setFieldValue('imageUrls', e.target.files);
   };
 
   const handleKeyDown = (event: KeyboardEvent<HTMLFormElement>) => {
@@ -76,7 +84,11 @@ export const NoteView = ({ uid, note, saveNote }: Props) => {
         </Typography>
       </Grid>
       <Grid item>
-        <IconButton color='primary'>
+        <IconButton
+          color='primary'
+          title='Subir imágenes'
+          data-message='Botón para subir imágenes.'
+          onClick={() => fileInputRef.current?.click()}>
           <UploadFileOutlined />
         </IconButton>
       </Grid>
@@ -87,7 +99,15 @@ export const NoteView = ({ uid, note, saveNote }: Props) => {
         onKeyDown={handleKeyDown}
         onSubmit={handleSubmit}
         noValidate>
-        <input type='file' multiple style={{ display: 'none' }} />
+        <input
+          name='imageUrls'
+          type='file'
+          accept='.jpg, .jpeg, .png'
+          multiple
+          ref={fileInputRef}
+          onChange={onFileInputChange}
+          style={{ display: 'none' }}
+        />
         <Grid item>
           <Button type='submit' color='primary' sx={{ padding: 2 }}>
             <SaveOutlined sx={{ fontSize: 30, mr: 1 }} />
